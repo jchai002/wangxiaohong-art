da98be189fd89d7771b2733d181596e60b8c8c58
Jerry Chai <jerry@gooddogdesign.com>
2016-10-23 14:17:04 -0700

    initial install

---
+++ craft/config/db.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Database Configuration
+ *
+ * All of your system's database configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ */
+ return array(
+     '*' => array(
+         'tablePrefix' => 'craft',
+         'server' => getenv('DB_HOST'),
+         'user' => getenv('DB_USER'),
+         'password' => getenv('DB_PASS'),
+         'database' => getenv('DB_NAME')
+     )
+ );
---
+++ public/gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minify = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
+
+var scssSource = './assets/scss/';
+var jsSource = './assets/js/source/';
+var jsDest = './assets/js/dest/';
+
+gulp.task('build-css', function() {
+  console.log('buidling css..')
+  gulp.src(scssSource + 'main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 25 versions']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./assets/css'))
+});
+
+gulp.task('build-js', function() {
+gulp.src([jsSource+'jQuery/*',jsSource+'core/tether.min.js',jsSource+'core/bootstrap.min.js',jsSource+'plugin/**/*',jsSource+'custom/**/*'])
+.pipe(concat('main.js'))
+.pipe(gulp.dest(jsDest));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(scssSource+ '**/*.scss', ['build-css']);
+  gulp.watch(jsSource +'**/*',['build-js']);
+});
+
+gulp.task('default', ['build-css', 'build-js', 'watch']);
