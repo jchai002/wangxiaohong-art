8c619aef8ad1ac98b8d335457d640b62faef8ee0
Jerry Chai <jerry@gooddogdesign.com>
2016-10-24 00:21:02 -0700

    add mina settings

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# A sample Gemfile
+source 'https://rubygems.org'
+
+gem 'mina'
+gem 'mina-slack', :git => 'git@github.com:johnkoht/mina-slack.git'
---
+++ config/deploy.rb
@@ -0,0 +1,112 @@
+require 'mina/git'
+​
+set :user, 'deploy'
+set :repository, 'jerrygit'
+set :project_name, "wangxiaohong.art"
+set :port, '22 -A'
+
+
+set :domain, '35.160.96.40'
+set :deploy_to, "/var/www/production.#{project_name}"
+set :branch, 'master'
+set :database, 'tla_production'
+set :shared_paths, ['craft/storage']
+​
+task :setup => :environment do
+  # create shared folders
+  queue! %[mkdir -p "#{deploy_to}/shared/craft/config"]
+  queue! %[mkdir -p "#{deploy_to}/shared/craft/storage"]
+  queue! %[mkdir -p "#{deploy_to}/shared/public/assets/images"]
+  queue! %[mkdir -p "#{deploy_to}/shared/public/assets/artifacts"]
+  queue! %[mkdir -p "#{deploy_to}/shared/public/assets/artifacts/image_files"]
+  queue! %[mkdir -p "#{deploy_to}/shared/public/assets/artifacts/video_files"]
+  queue! %[mkdir -p "#{deploy_to}/shared/public/assets/artifacts/audio_files"]
+  queue! %[mkdir -p "#{deploy_to}/shared/public/assets/artifacts/pdf_files"]
+  queue! %[mkdir -p "#{deploy_to}/shared/public/assets/artifacts/document_files"]
+  queue! %[mkdir -p "#{deploy_to}/shared/db_backup"]
+
+  # change project directory user group to web
+  queue! %[chgrp -R web "#{deploy_to}"]
+  # set project directory user permissions
+  queue! %[chmod -R 774 "#{deploy_to}"]
+  # make all new files and directories created under project directory inherit the same permissions
+  queue! %[chmod g+s "#{deploy_to}"]
+end
+​
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'deploy:cleanup'
+  end
+end
+
+# db relate tasks
+desc "backup remote database, then sync local db to remote"
+task :dbsync => :environment do
+  invoke :db_backup
+  invoke :db_transfer
+  invoke :db_overwrite
+  invoke :clean_up
+end
+
+task :db_backup do
+  queue! %[mysqldump -u root -p#{ENV['pw']} "#{database}" > "#{deploy_to}/db_backup/backup_#{Time.now.to_s.gsub(/\W/,'_')}.sql"]
+end
+
+task :db_transfer do
+  system "/Applications/MAMP/Library/bin/mysqldump -u root -proot tla > dump.sql
+          scp dump.sql deploy@#{domain}:#{deploy_to}/shared"
+end
+
+task :db_overwrite do
+  queue! %[cd "#{deploy_to}/shared"]
+  queue! %[mysql -u root -p#{ENV['pw']} hong < dump.sql]
+end
+
+task :clean_up do
+  queue! %[cd "#{deploy_to}/shared"]
+  queue! %[rm dump.sql]
+  system "rm dump.sql"
+end
+
+task :db_dump do
+  queue! %[mysqldump -u root -p#{ENV['pw']} "#{database}" > #{deploy_to}/dump.sql]
+end
+
+task :db_pull do
+  system "scp deploy@#{domain}:#{deploy_to}/dump.sql dump.sql"
+  system "#{ENV['command'] || '/Applications/MAMP/Library/bin/mysql'} -u root -proot tla < dump.sql"
+end
+
+task :upload_images do
+  system "scp -r /Applications/MAMP/htdocs/TLA-009-TLA-Teaching-and-Learning-Practices/public/assets/images deploy@#{domain}:#{deploy_to}/shared/public/assets/"
+  queue! %[chgrp -R web #{deploy_to}/shared/public/assets/images]
+  queue! %[chmod -R 774 "#{deploy_to}/shared/public/assets/images"]
+end
+
+task :download_images do
+  system "scp -r deploy@#{domain}:#{deploy_to}/shared/public/assets/images /Applications/MAMP/htdocs/TLA-009-TLA-Teaching-and-Learning-Practices/public/assets/"
+end
+
+task :download_artifacts do
+  system "scp -r deploy@#{domain}:#{deploy_to}/shared/public/assets/artifacts /Applications/MAMP/htdocs/TLA-009-TLA-Teaching-and-Learning-Practices/public/assets/"
+end
+
+task :download_assets do
+  invoke :download_images
+  invoke :download_artifacts
+end
+
+task :db_dump_transfer do
+  queue! %[sh #{deploy_to}/scripts/db_dump_transfer.sh]
+end
+
+task :db_inject do
+  queue! %[sh #{deploy_to}/scripts/db_inject.sh]
+end
+
+task :assets_staging_to_production do
+    queue! %[sh #{deploy_to}/scripts/assets_staging_to_production.sh]
+end
